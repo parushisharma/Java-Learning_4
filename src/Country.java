47f027f33b1195fa6131bed9093e1f1dcd75fcd5
parushi <parushi@10.0.0.12>
2016-07-05 23:34:53 -0700

    Intial commit

---
+++ src/Country.java
@@ -0,0 +1,23 @@
+
+public class Country {
+
+	public Country(String string, int numberOfYears) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addSubscriptionYear(int i, double countryData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double getNumSubscriptionsForPeriod(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
