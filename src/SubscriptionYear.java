1efb1120fa27505e0abf6edde14da9cbc3f020fe
parushi <parushi@10.0.0.12>
2016-07-07 23:03:44 -0700

    Proper comments

--- src/SubscriptionYear.java
+++ src/SubscriptionYear.java
@@ -1,37 +1,68 @@
+/*
+ * This is a class called SubscriptionYear that contains getters
+ * and setters to retrieve required information. 
+ * 
+ */
+
 public class SubscriptionYear {
-	
+	// Instances are private to the user. 
 	private int year;
 	private double subscriptions;
-	//SubscriptionYear next;
 	
 	
-	//stores the year and it's statistical data.
+	/*
+	 *  Constructor method 
+	 *  This method stores the year and it's statistical data.
+	 *  
+	 *  @param year						Integer of the input year
+	 *  @param subscriptions 			value (double) of the subscriptions
+	 */
 	public SubscriptionYear(int year,double subscriptions) {
 	    setYear(year);
 	    setSubscription(subscriptions);
-	   // this.next = null;
 	}
 	
-	//sets the year
+	/*
+	 * This method sets the year
+	 * 
+	 * @param Year 						Integer of the input year 
+	 */
 	public void setYear(int Year) {
 	    this.year= Year;
 	}
 	
-	//sets the subscriptions .
+	/*
+	 * This method sets the subscriptions 
+	 * 
+	 * @param value 					The value of the subscription
+	 */
 	public void setSubscription(double value) {
 	    this.subscriptions = value;
 	}
 	
-	//gets the year
+	/*
+	 * This method gets the year
+	 * 
+	 * @return the year
+	 */
 	public int getYear() {
 	    return year;
 	}   
 	
-	 //gets the subscriptions 
+	/*
+	 * This method gets the subscriptions 
+	 * 
+	 * @return subscriptions 
+	 */
 	public double getSubscription() {
 	    return subscriptions;
 	}
 	
+	/*
+	 * This method prints the value required 
+	 * 
+	 * @return string and subscriptions 
+	 */
 	public String toString() {
 	    return "Number of Subscriptions: "+ subscriptions;
 	}
